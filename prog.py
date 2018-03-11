8b12a1ea9a36c1ea42ea837770a88f19f81bea38
Subha Chakraborty <1997subhockb@gmail.com>
2018-03-11 10:00:32 +0530

    Add files via upload
    script of the password generator

---
+++ prog.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+#length of password
+z=int(input('What is the length of your password? '))
+
+#function to generate password containing alphanumeric string and punctuations
+def gen(size = 10, chars=string.ascii_letters + string.digits + string.punctuation):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+print(gen(z))
